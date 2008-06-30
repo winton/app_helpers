7cb25812ee314cfbb8f32cefd035d39114c116cf
Winton Welsh <win@sumapp.com>
2008-06-30 11:42:42 -0500

    ActionController::Base.view_paths doesn't support push in 2.1

--- lib/app_helpers.rb
+++ lib/app_helpers.rb
@@ -3,7 +3,7 @@ Dir[File.expand_path('*/*.rb', File.dirname(__FILE__))].each do |f|
 end
 
 ActionView::Base.send :include, AppHelpers
-ActionController::Base.view_paths.push RAILS_ROOT + '/app/widgets'
+ActionController::Base.view_paths += RAILS_ROOT + '/app/widgets'
 ActionController::Base.class_eval do
   public :render_to_string
 end
