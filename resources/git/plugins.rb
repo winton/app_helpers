6700f16f6ab931e51230a0cc8dea0c217f2f6af6
Winton Welsh <win@sumapp.com>
2008-09-05 10:06:47 -0500

    Updating default plugins.rb file

--- resources/git/plugins.rb
+++ resources/git/plugins.rb
@@ -5,11 +5,11 @@
   { :repo => 'git@github.com:winton/rails_widget.git' },
   { :repo => 'git@github.com:winton/widgets.git', :to => 'app/widgets/widgets' },
   { :repo => 'git://github.com/mislav/will_paginate.git', :depth => 1, :tag => '2.3.2' },
-  { :repo => 'git://github.com/rails/rails.git', :to => 'vendor/rails', :depth => 1 },
   'haml'
 ]
-#git@github.com:winton/captcha.git
-#git@github.com:winton/admin.git
-#git://github.com/technoweenie/attachment_fu.git
-#git://github.com/jcnetdev/active_record_without_table.git
-#git://github.com/freelancing-god/thinking-sphinx.git
+#{ :repo => 'git://github.com/rails/rails.git', :to => 'vendor/rails', :depth => 1 }
+#{ :repo => 'git@github.com:winton/captcha.git' }
+#{ :repo => 'git@github.com:winton/admin.git' }
+#{ :repo => 'git://github.com/technoweenie/attachment_fu.git' }
+#{ :repo => 'git://github.com/jcnetdev/active_record_without_table.git' }
+#{ :repo => 'git://github.com/freelancing-god/thinking-sphinx.git' }
